c8e3624afd0216892a8a4372a9ec438d7b0cad7a
Milana <milanak7797@gmail.com>
2020-04-23 14:51:14 +0200

    Scripts for basic audio(player mechanics) exist and work.
    Walk, Jump, Land and Slide(even slide on the wall)

---
+++ Project Dream/Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    PlayerState playerState;
+
+    [FMODUnity.EventRef]
+    public string Jump = "event:/Music/Jumping";
+
+    [FMODUnity.EventRef]
+    public string Land = "event:/Music/Landing";
+
+   
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        playerState = GameObject.Find("PlayerChar").GetComponent<PlayerState>();
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+            
+
+    }
+
+    void PlayJump()
+    {
+       
+    }
+
+    void PlayLand()
+    {
+        
+    }
+
+    void PlayWalk()
+    {
+
+    }
+
+    void PlaySlide()
+    {
+
+    }
+
+
+}
--- Project Dream/Assets/Scripts/Audio/Jump.cs
+++ Project Dream/Assets/Scripts/Audio/Jump.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class Jump : MonoBehaviour
 {
-    PlayerState playerState;
-
+    PlayerState player;
+ 
     [FMODUnity.EventRef]
     public string selectedSound;
-    FMOD.Studio.EventInstance soundEvent;
+    //FMOD.Studio.EventInstance soundEvent;
+    
+    bool playsound;
 
     
 
@@ -16,22 +18,40 @@ public class Jump : MonoBehaviour
     void Start()
     {
       
-        playerState = GameObject.Find("Player").GetComponent<PlayerState>();
-
+        player = GameObject.Find("PlayerChar").GetComponent<PlayerState>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        FMODUnity.RuntimeManager.AttachInstanceToGameObject(soundEvent, GetComponent<Transform>(), GetComponent<Rigidbody>());
+        //FMODUnity.RuntimeManager.AttachInstanceToGameObject(soundEvent, GetComponent<Transform>(), GetComponent<Rigidbody>());
 
-        if (playerState.isJumping == true)
+
+        if (player.isJumping && playsound)
         {
-            soundEvent = FMODUnity.RuntimeManager.CreateInstance(selectedSound);
-            soundEvent.start();
-        
 
+            playsound = false;
+            JumpSound();         
+             
         }
+
+        if (!player.isJumping && player.isTouchingGround)
+        {
+            playsound = true;
+            
+        }
+
+    }
+
+    void JumpSound()
+    {
+
+        FMODUnity.RuntimeManager.PlayOneShot(selectedSound, GetComponent<Transform>().position); //PlayOneShotAttached
+
+        //soundEvent = FMODUnity.RuntimeManager.CreateInstance(selectedSound);
+        //    soundEvent.start();
+                   
     }
 
 
