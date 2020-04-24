327aa0f54024f53eab3017babdd954b4259cd889
max <max.hemberg@gmail.com>
2020-04-24 18:12:31 +0200

    SceneSwapping Enabled
    Button Works on Mobile Device

---
+++ Project Dream/Assets/Button_HUD.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Button_HUD : MonoBehaviour
+{
+    public GameObject buttonEvent;
+
+    private void Update()
+    {
+        if (buttonEvent.activeInHierarchy) {
+            Time.timeScale = 0f;
+        } else {
+            Time.timeScale = 1f;
+        }
+    }
+
+    public void BackButtonPress()
+    {
+        buttonEvent.SetActive(true);
+    }
+
+  public void BackToMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void BackButtonNO()
+    {
+        buttonEvent.SetActive(false);
+    }
+}
