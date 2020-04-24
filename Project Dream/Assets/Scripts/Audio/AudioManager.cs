17002c737ece592a299ccc26d1e180ef91d275f4
Milana <milanak7797@gmail.com>
2020-04-24 21:45:16 +0200

    Latest Audio Scripts+Files

--- Project Dream/Assets/Scripts/Audio/AudioManager.cs
+++ Project Dream/Assets/Scripts/Audio/AudioManager.cs
@@ -7,13 +7,25 @@ public class AudioManager : MonoBehaviour
     PlayerState playerState;
 
     [FMODUnity.EventRef]
-    public string Jump = "event:/Music/Jumping";
+    public string Jumping = "event:/SFX/Jumping";
 
     [FMODUnity.EventRef]
-    public string Land = "event:/Music/Landing";
+    public string Landing = "event:/SFX/Landing";
 
-   
-   
+    [FMODUnity.EventRef]
+    public string Walking = "event:/SFX/Walking";
+
+    [FMODUnity.EventRef]
+    public string Sliding = "event:/SFX/Sliding";
+
+    FMOD.Studio.EventInstance soundEvent;
+
+    //Variables to keep track of what sounds are allowed to be played atm
+
+    bool inAir;
+    bool onGround;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -26,28 +38,95 @@ public class AudioManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-            
+        PlayJumpSFX();
+        PlayLandingSFX();
+        PlayWalkingSFX();
+        PlaySlidingSFX();
 
-    }
-
-    void PlayJump()
-    {
-       
-    }
-
-    void PlayLand()
-    {
         
     }
 
-    void PlayWalk()
+    void PlayJumpSFX()
     {
+
+        if (!playerState.isJumping && playerState.isTouchingGround)
+        {
+            onGround = true;
+
+        }
+        else if(!playerState.isTouchingGround)
+        {
+            onGround = false;
+        }
+           
+
+        if (playerState.isJumping && onGround)
+        {
+
+            print("jump sound");
+            FMODUnity.RuntimeManager.PlayOneShot(Jumping, GetComponent<Transform>().position);
+            onGround = false;
+            
+         
+        }
+
+        
+
 
     }
 
-    void PlaySlide()
+    void PlayLandingSFX()
     {
 
+        if (!playerState.isTouchingGround)
+        {
+            inAir = true;
+        }
+
+        if (inAir)
+        {
+            if (playerState.isTouchingGround)
+            {
+
+                inAir = false;
+                FMODUnity.RuntimeManager.PlayOneShot(Landing, GetComponent<Transform>().position);
+                
+            }
+        }
+
+
+    }
+
+    void PlayWalkingSFX()
+    {
+
+        
+        FMODUnity.RuntimeManager.AttachInstanceToGameObject(soundEvent, GetComponent<Transform>(), GetComponent<Rigidbody>());
+
+
+
+        if (playerState.isRunning)
+        {
+            
+            soundEvent = FMODUnity.RuntimeManager.CreateInstance(Walking);
+            soundEvent.start();
+            soundEvent.release();
+
+        }
+
+    }
+
+    void PlaySlidingSFX()
+    {
+        FMODUnity.RuntimeManager.AttachInstanceToGameObject(soundEvent, GetComponent<Transform>(), GetComponent<Rigidbody>());
+
+        if (playerState.isSliding || playerState.isWallSliding)
+        {
+            soundEvent = FMODUnity.RuntimeManager.CreateInstance(Landing);
+            soundEvent.start();
+            soundEvent.release();
+
+        }
     }
 
 
---
+++ Project Dream/Assets/Scripts/Audio/Basics/Land.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Land : MonoBehaviour
+{
+    PlayerState playerState;
+
+    [FMODUnity.EventRef]
+    public string selectedSound;
+    //FMOD.Studio.EventInstance soundEvent;
+
+    bool inAir;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        playerState = GameObject.Find("PlayerChar").GetComponent<PlayerState>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //FMODUnity.RuntimeManager.AttachInstanceToGameObject(soundEvent, GetComponent<Transform>(), GetComponent<Rigidbody>());
+
+        if (!playerState.isTouchingGround)
+        {
+            inAir = true;
+        }
+
+        if (inAir)
+        {
+            if (playerState.isTouchingGround)
+            {
+                
+                    inAir = false;
+                FMODUnity.RuntimeManager.PlayOneShot(selectedSound, GetComponent<Transform>().position);
+                //soundEvent = FMODUnity.RuntimeManager.CreateInstance(selectedSound);
+                //    soundEvent.start();
+                
+            }
+        }
+    }
+
+    
+
+
+}
