e926a19d57a0c864bf9a5b3f29e8d100bbc62647
max <max.hemberg@gmail.com>
2020-04-27 19:09:10 +0200

    Implemented Basic One-Way Elevator

---
+++ Project Dream/Assets/ElevatorMover.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElevatorMover : MonoBehaviour
+{
+    bool elevatorMoving = false;
+    bool elevatorTriggered = false;
+
+    public float elevatorSpeed;
+    Transform originTransform;
+    public Transform targetTransform;
+
+    int direction;
+   
+    void Start()
+    {
+        originTransform = this.transform;
+    }
+
+
+    void Update()
+    {
+        if(transform.position.y == originTransform.position.y) {
+            direction = -1;
+        } else if (transform.position.y == targetTransform.position.y) {
+            direction = 1;
+        }
+
+        if (Input.GetKey(KeyCode.E)) { //Player triggered elevator movement.
+            elevatorTriggered = true;
+        }
+
+        if(transform.position != originTransform.position || transform.position != targetTransform.position) { //Elevator currently moving.
+            elevatorMoving = true;
+        } else {
+            elevatorMoving = false;
+        }
+
+        if(elevatorMoving && transform.position.y <= targetTransform.position.y) { //Elevator reached destination.
+            elevatorTriggered = false;
+        }
+
+        if (elevatorTriggered) { //Makes Elevator Move
+            transform.Translate(0f, elevatorSpeed * direction * Time.deltaTime, 0f);
+        }
+
+
+      
+    }
+}
