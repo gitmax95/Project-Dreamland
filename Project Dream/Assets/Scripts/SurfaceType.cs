c4504b7fd96b03c65e560967d48598df0fdc82a4
Ludde <ludden1996@outlook.com>
2020-05-07 13:46:48 +0200

    Updated Textured Assets

---
+++ Project Dream/Assets/Scripts/SurfaceType.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SurfaceType : MonoBehaviour
+{
+
+    enum SurfaceTypes { Stone, Carpet, Wood};
+    
+    [SerializeField]
+    SurfaceTypes thisSurface;
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+   public string getType()
+    {
+        if(thisSurface == SurfaceTypes.Stone)
+        {
+            return "Stone";
+        }
+        else if (thisSurface == SurfaceTypes.Carpet)
+        {
+            return "Carpet";
+        }
+        else if (thisSurface == SurfaceTypes.Wood)
+        {
+            return "Wood";
+        }
+
+        return "idk";
+    }
+}
