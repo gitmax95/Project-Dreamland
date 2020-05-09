7b4cafe74ac262bd87bdc2fc350e1abd8bc86d58
Milana <milanak7797@gmail.com>
2020-05-09 19:45:39 +0200

    Saw prefab modified(3D SFX working)

--- Project Dream/Assets/Scripts/Audio/ObjectSFX/SawSFX.cs
+++ Project Dream/Assets/Scripts/Audio/ObjectSFX/SawSFX.cs
@@ -4,29 +4,23 @@ using UnityEngine;
 
 public class SawSFX : MonoBehaviour
 {
-    SawController sawController;
-
+    SawMovement sawM;
     Transform saw;
 
     [FMODUnity.EventRef] string Saw = "event:/SFX/Saw";
 
-    FMOD.Studio.EventInstance soundEvent;
-
     // Start is called before the first frame update
     void Start()
     {
-        saw = this.gameObject.transform.GetChild(1);
-        sawController = this.gameObject.GetComponent<SawController>();
+        saw = this.gameObject.transform;
+        sawM = this.gameObject.GetComponent<SawMovement>();
 
-        soundEvent = FMODUnity.RuntimeManager.CreateInstance(Saw);
-        soundEvent.start();
-        soundEvent.release();
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        FMODUnity.RuntimeManager.PlayOneShot(Saw, saw.position);
     }
 }
