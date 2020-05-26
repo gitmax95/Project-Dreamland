0bcc3b26b281741258a0429d0ac08a57eb800040
Milana <milanak7797@gmail.com>
2020-05-26 13:45:58 +0200

    Bell, Spike SFX and Canon SFX

---
+++ Project Dream/Assets/Scripts/Audio/ObjectSFX/WallSpikeDamageSFX.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallSpikeDamageSFX : MonoBehaviour
+{
+    [FMODUnity.EventRef]
+    public string spikeDamage = "event:/SFX/SpikeDamage";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            FMODUnity.RuntimeManager.PlayOneShot(spikeDamage, GetComponent<Transform>().position);
+        }
+
+    }
+}
